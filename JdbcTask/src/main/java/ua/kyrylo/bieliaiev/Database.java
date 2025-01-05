0438082bff0b6d2a2a017a3b005a8def738e2011
Kyrylo Bieliaiev <kirillbelyaev2921@gmail.com>
2025-01-05 13:22:31 +0200

    feat: add DatabaseInitService

---
+++ JdbcTask/src/main/java/ua/kyrylo/bieliaiev/Database.java
@@ -0,0 +1,28 @@
+package ua.kyrylo.bieliaiev;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static Database instance = null;
+
+    private final Connection connection;
+
+    private Database() throws SQLException {
+        String url = "jdbc:h2:mem:test";
+        connection = DriverManager.getConnection(url);
+    }
+
+    public static Database getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
