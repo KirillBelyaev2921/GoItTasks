5795f2a47966dc97cd23d4002691f061d62d8bbf
Kyrylo Bieliaiev <kirillbelyaev2921@gmail.com>
2025-02-18 14:05:16 +0200

    feat: add timezone to TimeServlet

--- ServletTask/src/main/java/ua/kyrylo/bieliaiev/TimeServlet.java
+++ ServletTask/src/main/java/ua/kyrylo/bieliaiev/TimeServlet.java
@@ -7,19 +7,32 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
 
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss OOOO");
+    private static final Pattern timezonePattern = Pattern.compile("UTC[ +]?(-?\\d+)");
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
-        String resultTime = now.format(formatter) + " UTC";
+        String timezone = req.getParameter("timezone");
+        int offset = 0;
+        if (timezone != null) {
+            Matcher matcher = timezonePattern.matcher(timezone);
+            if (matcher.find()) {
+                offset = Integer.parseInt(matcher.group(1));
+            } else {
+                throw new ServletException("Invalid timezone: " + timezone);
+            }
+        }
+        ZonedDateTime now = ZonedDateTime.now(ZoneOffset.ofHours(offset));
+        String resultTime = now.format(formatter);
 
         resp.setContentType("text/html; charset=utf-8");
         resp.getWriter().write(resultTime);
